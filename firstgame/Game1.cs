2ea1a4bcfac71f50aa075d52467c1d312dc8cf7f
KalamitousN <nkcrit@gmail.com>
2024-02-23 10:29:26 +0100

    First Push (real)
    Hope this works?

--- firstgame/Game1.cs
+++ firstgame/Game1.cs
@@ -12,13 +12,12 @@ namespace firstgame
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
         Texture2D floorTile;
-        int gameWidth;
-        int gameHeight;
-        int playerX;
-        int playerY;
-        int cameraX;
-        int cameraY;
-            
+        Texture2D playerPlaceholder;
+        int gameWidth, gameHeight;
+        int cameraX, cameraY, playerCenterX, playerCenterY;
+        int playerX = 0, playerY = -960;
+        float playerYPhysics = 0, playerXPhysics = 0;
+
         public Game1()
         {
             _graphics = new GraphicsDeviceManager(this);
@@ -66,8 +65,9 @@ namespace firstgame
             _spriteBatch = new SpriteBatch(GraphicsDevice);
 
             floorTile = Content.Load<Texture2D>("bricktexture");
+            playerPlaceholder = Content.Load<Texture2D>("playerbasic");
 
-            // load game content here
+            // load game content
         }
 
         protected override void Update(GameTime gameTime)
@@ -78,18 +78,50 @@ namespace firstgame
             if (state.IsKeyDown(Keys.Escape))
                 Exit();
 
-            if (state.IsKeyDown(Keys.W))
-                cameraY++;
-            if (state.IsKeyDown(Keys.S))
-                cameraY--;
             if (state.IsKeyDown(Keys.A))
-                cameraX++;
+                playerXPhysics = -5f;
             if (state.IsKeyDown(Keys.D))
-                cameraX--;
+                playerXPhysics = 5f;
+            if (playerXPhysics != 0)
+                if (playerY > -65)
+                {
+                    playerXPhysics = playerXPhysics / 1.2f;
+                }
+                else
+                {
+                    playerXPhysics = playerXPhysics / 1.02f;
+                }
+            playerX += (int)playerXPhysics;
+
+            if (playerY > -65)
+            {
+                playerY = -64;
+                playerYPhysics = 0;
+                if (state.IsKeyDown(Keys.Space))
+                {
+                    playerYPhysics = -8;
+                    playerY -= 8;
+                }
+            }
+            else
+            {
+                playerYPhysics += 0.3f;
+                if (state.IsKeyDown(Keys.Space) == false && playerYPhysics < -4)
+                    playerYPhysics = -4;
+
+                if (playerYPhysics > 12)
+                    playerYPhysics = 12;
+                playerY += (int)(playerYPhysics);
+            }
 
             if (cameraY < gameHeight/2 + 32)
                 cameraY = gameHeight/2 + 32;
-          
+
+            playerCenterX = -playerX + gameWidth / 2 - 18;
+            playerCenterY = -playerY + gameHeight / 2 - 12;
+            cameraX = playerCenterX - (int)playerXPhysics;
+            cameraY = playerCenterY - (int)playerYPhysics;
+
             // game updates
 
             base.Update(gameTime);
@@ -107,7 +139,11 @@ namespace firstgame
                     _spriteBatch.Draw(floorTile, new Rectangle(x * 16 + (int)(cameraX % 16), y * 16 + (int)(cameraY), floorTile.Width * 2, floorTile.Height * 2), new Color(1f -((float)y)/32f, 1f - ((float)y)/32f, 1f - ((float)y)/32f, 1));
                 };
             }
+
+            _spriteBatch.Draw(playerPlaceholder, new Rectangle(playerX + cameraX, playerY + cameraY, playerPlaceholder.Width * 2, playerPlaceholder.Height * 2), Color.White);
+
             _spriteBatch.End();
+
             
             // rendering
 
